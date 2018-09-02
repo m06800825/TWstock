2de6ad56bc2ef18bfb1b05bc7283a2ec4804f37e
Kai-Yuan, Cheng <40761610+m06800825@users.noreply.github.com>
2018-09-02 21:34:25 +0800

    20180902

--- main_crawler.py
+++ main_crawler.py
@@ -8,11 +8,14 @@ Created on Sat Sep  1 17:36:26 2018
 import datetime
 from pack import crawler
 
-x = datetime.date.today() - datetime.timedelta(days=1)
+x = datetime.date.today() - datetime.timedelta(days=2)
 year = x.year
 month = x.month
 day = x.day
 
 a = crawler.Crawl(year, month, day)
-a.write_sql()
+a.write_sql_dailyprice()
 print(a.today_choice())
+
+b = crawler.Crawl(2018, 3, 1)
+print(b.crawl_monthly_report())
--- pack/crawler.py
+++ pack/crawler.py
@@ -40,7 +40,7 @@ class Crawl():
         df = df.set_index(['stock_id', 'date'])
         df = df.apply(lambda s:pd.to_numeric(s, errors='coerce'))
         df = df[df.columns[df.isnull().all() == False]]
-    
+        
         return df
     
     
@@ -49,10 +49,51 @@ class Crawl():
         close_open = dft['收盤價'] / dft['開盤價']
 
         dff = dft[(close_open>1.03) & (dft['本益比']<20.0) & (dft['本益比']>0.0) &(dft['成交筆數']>500)]
+        
         return dff[['開盤價','收盤價','本益比']]
     
     
-    def write_sql(self):
+    def write_sql_dailyprice(self):
         conn = sqlite3.connect('stock.sqlite3')
         self.crawl_price().to_sql(('daily_price'+"_"+str(self.y)+"-"+str(self.m)+"-"+str(self.d)), conn, if_exists='replace')
+        
+    
+    def crawl_monthly_report(self):
+        date = datetime.date(self.y,self.m,self.d)
+        url = 'http://mops.twse.com.tw/nas/t21/sii/t21sc03_'+str(date.year - 1911)+'_'+str(date.month)+'_0.html'
+        if date.year - 1991 <= 98: #民國98年以前的月報爬取網址不一樣
+            url = 'http://mops.twse.com.tw/nas/t21/sii/t21sc03_'+str(date.year - 1911)+'_'+str(date.month)+'.html'
+    
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+ 
+        r = requests.get(url, headers)
+        r.encoding = 'big5'
+        
+        try:
+            html_df = pd.read_html(StringIO(r.text))
+        except:
+            return None
+
+        if len(html_df[0]) > 500:
+            df = html_df[0].copy()
+        else:
+            df = pd.concat([df for df in html_df if df.shape[1] <= 11])
+
+        df = df[list(range(0,10))]
+        column_index = df.index[(df[0] == '公司代號')][0]
+        df.columns = df.iloc[column_index]
+        df['當月營收'] = pd.to_numeric(df['當月營收'], errors='coerce')
+        df = df[~df['當月營收'].isnull()]
+        df = df[df['公司代號'] != '合計']
+        next_month = datetime.date(date.year + int(date.month / 12), ((date.month % 12) + 1), 10)
+        df['date'] = pd.to_datetime(next_month)
+        df = df.rename(columns={'公司代號':'stock_id'})
+        df = df.set_index(['stock_id', 'date'])
+        df = df.apply(lambda s:pd.to_numeric(s, errors='coerce'))
+        df = df[df.columns[df.isnull().all() == False]]
+        
+        conn = sqlite3.connect('stock.sqlite3')
+        df.to_sql('monthly_report'+"_"+str(self.y)+"-"+str(self.m), conn, if_exists='replace')
+        
+        return df
         
